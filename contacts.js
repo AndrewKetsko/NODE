29723af99d3a72c5947c4520a6a7fc02cd1741f8
Andrew Ketsko <monicman82@gmail.com>
2023-06-19 17:36:18 +0300

    first commit

---
+++ contacts.js
@@ -0,0 +1,60 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(contacts);
+};
+
+const getContactById = async (id) => {
+  const data = await listContacts();
+  return data.find((contact) => contact.id === id);
+};
+
+// const getContact = async (data) => {
+//   const { id = null, name = null, email = null, phone = null } = data;
+//   searchParam = id
+//     ? "id"
+//     : name
+//     ? "name"
+//     : email
+//     ? "email"
+//     : phone
+//     ? "phone"
+//     : null;
+//   if (!searchParam) return "no params to find contact";
+//   const fileData = await fs.readFile(contactsPath, "utf-8");
+//   const contact = JSON.parse(fileData).filter(
+//     (contact) => contact[searchParam] === data[searchParam]
+//   );
+//   if (contact.length === 0) return "there are no contact with this params";
+//   return contact;
+// };
+
+const removeContact = async (id) => {
+  const data = await listContacts();
+  const index = data.findIndex((contact) => contact.id === id);
+  if (index === -1) return null;
+  const [removed] = data.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+  return removed;
+};
+
+const addContact = async (contact) => {
+  const newContact = { ...contact, id: nanoid() };
+  const data = await listContacts();
+  data.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  //   getContact,
+  removeContact,
+  addContact,
+};
---
+++ index.js
@@ -0,0 +1,56 @@
+const contacts = require("./contacts");
+// const yargs = require("yargs");
+// const { hideBin } = require("yargs/helpers");
+
+const { Command } = require("commander");
+const program = new Command();
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+const argv = program.opts();
+
+
+const invokeAction = async ({
+  action,
+  id = null,
+  name = null,
+  email = null,
+  phone = null,
+}) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+    case "get":
+      const contact = await contacts.getContactById(id);
+      return console.log(contact);
+    case "remove":
+      const result = await contacts.removeContact(id);
+      return console.log(result);
+    case "add": {
+      const result = await contacts.addContact({ name, email, phone });
+      return console.log(result);
+    }
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+      break;
+  }
+};
+
+// invokeAction({ action: "list" });
+// invokeAction({ action: "get", id: "e6ywwRe4jcqxXfCZOj_1e" });
+// invokeAction({ action: "remove", id: "wEDQhnEQqAhFO-vcon_gW" });
+// invokeAction({ action: "add", name: "Andrew", email: "a@a.a", phone: "020000333222" });
+
+// const res = hideBin(process.argv);
+// const { argv } = yargs(res);
+// console.log(argv);
+// invokeAction(argv);
+
+invokeAction(argv);
